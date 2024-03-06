aa6f1404cf33cffe0c98c0c98724159728a6b45e
SAKSITHON\saksi <saksithon.mt@technictrang.ac.th>
2024-03-06 15:24:39 +0700

    Refactor account settings page and add user data fetching

--- src/views/account-settings/TabAccount.tsx
+++ src/views/account-settings/TabAccount.tsx
@@ -1,165 +1,199 @@
 // ** React Imports
-import { useState, ElementType, ChangeEvent, SyntheticEvent } from 'react'
+import {
+  useState,
+  ElementType,
+  ChangeEvent,
+  useEffect,
+} from "react";
+import { Controller, useForm } from "react-hook-form"; // Import Controller and useForm from react-hook-form
 
 // ** MUI Imports
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
-import Link from '@mui/material/Link'
-import Alert from '@mui/material/Alert'
-import Select from '@mui/material/Select'
-import { styled } from '@mui/material/styles'
-import MenuItem from '@mui/material/MenuItem'
-import TextField from '@mui/material/TextField'
-import Typography from '@mui/material/Typography'
-import InputLabel from '@mui/material/InputLabel'
-import AlertTitle from '@mui/material/AlertTitle'
-import IconButton from '@mui/material/IconButton'
-import CardContent from '@mui/material/CardContent'
-import FormControl from '@mui/material/FormControl'
-import Button, { ButtonProps } from '@mui/material/Button'
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
 
-// ** Icons Imports
-import Close from 'mdi-material-ui/Close'
+import Button, { ButtonProps } from "@mui/material/Button";
 
-const ImgStyled = styled('img')(({ theme }) => ({
+
+import { User as UserData } from "@/interfaces/User.interface";
+
+const ImgStyled = styled("img")(({ theme }) => ({
   width: 120,
   height: 120,
   marginRight: theme.spacing(6.25),
-  borderRadius: theme.shape.borderRadius
-}))
+  borderRadius: theme.shape.borderRadius,
+}));
 
-const ButtonStyled = styled(Button)<ButtonProps & { component?: ElementType; htmlFor?: string }>(({ theme }) => ({
-  [theme.breakpoints.down('sm')]: {
-    width: '100%',
-    textAlign: 'center'
-  }
-}))
+const ButtonStyled = styled(Button)<
+  ButtonProps & { component?: ElementType; htmlFor?: string }
+>(({ theme }) => ({
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
+    textAlign: "center",
+  },
+}));
 
 const ResetButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
   marginLeft: theme.spacing(4.5),
-  [theme.breakpoints.down('sm')]: {
-    width: '100%',
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
     marginLeft: 0,
-    textAlign: 'center',
-    marginTop: theme.spacing(4)
-  }
-}))
+    textAlign: "center",
+    marginTop: theme.spacing(4),
+  },
+}));
 
-const TabAccount = () => {
-  // ** State
-  const [openAlert, setOpenAlert] = useState<boolean>(true)
-  const [imgSrc, setImgSrc] = useState<string>('/images/avatars/1.png')
+type TabAccountProps = {
+  User?: UserData;
+  onSaved?: (data: UserData) => void;
+};
+
+const TabAccount = ({ User,onSaved }: TabAccountProps) => {
+  const { control, handleSubmit, reset } = useForm(); // Initialize useForm hook
+
+  const [imgSrc, setImgSrc] = useState<string>("/images/avatars/1.png");
 
   const onChange = (file: ChangeEvent) => {
-    const reader = new FileReader()
-    const { files } = file.target as HTMLInputElement
+    const reader = new FileReader();
+    const { files } = file.target as HTMLInputElement;
     if (files && files.length !== 0) {
-      reader.onload = () => setImgSrc(reader.result as string)
+      reader.onload = () => setImgSrc(reader.result as string);
 
-      reader.readAsDataURL(files[0])
+      reader.readAsDataURL(files[0]);
     }
-  }
+  };
+
+  const onSubmit = (data: UserData) => {
+    onSaved(data);
+  };
+
+  useEffect(() => {
+    if (User) {
+      reset(User);
+    }
+  }, []);
 
   return (
     <CardContent>
-      <form>
+      {/* {JSON.stringify(User?.firstName)} */}
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container spacing={7}>
           <Grid item xs={12} sx={{ marginTop: 4.8, marginBottom: 3 }}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <ImgStyled src={imgSrc} alt='Profile Pic' />
-              <Box>
-                <ButtonStyled component='label' variant='contained' htmlFor='account-settings-upload-image'>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <ImgStyled src={imgSrc} alt="Profile Pic" />
+              {/* <Box>
+                <ButtonStyled
+                  component="label"
+                  variant="contained"
+                  htmlFor="account-settings-upload-image"
+                >
                   Upload New Photo
                   <input
                     hidden
-                    type='file'
+                    type="file"
                     onChange={onChange}
-                    accept='image/png, image/jpeg'
-                    id='account-settings-upload-image'
+                    accept="image/png, image/jpeg"
+                    id="account-settings-upload-image"
                   />
                 </ButtonStyled>
-                <ResetButtonStyled color='error' variant='outlined' onClick={() => setImgSrc('/images/avatars/1.png')}>
+                <ResetButtonStyled
+                  color="error"
+                  variant="outlined"
+                  onClick={() => setImgSrc("/images/avatars/1.png")}
+                >
                   Reset
                 </ResetButtonStyled>
-                <Typography variant='body2' sx={{ marginTop: 5 }}>
+                <Typography variant="body2" sx={{ marginTop: 5 }}>
                   Allowed PNG or JPEG. Max size of 800K.
                 </Typography>
-              </Box>
+              </Box> */}
             </Box>
           </Grid>
 
           <Grid item xs={12} sm={6}>
-            <TextField fullWidth label='Username' placeholder='johnDoe' defaultValue='johnDoe' />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField fullWidth label='Name' placeholder='John Doe' defaultValue='John Doe' />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              type='email'
-              label='Email'
-              placeholder='johnDoe@example.com'
-              defaultValue='johnDoe@example.com'
+            <Controller
+              name="firstName"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  label="First Name"
+                  placeholder=""
+                  autoFocus
+                />
+              )}
             />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <FormControl fullWidth>
-              <InputLabel>Role</InputLabel>
-              <Select label='Role' defaultValue='admin'>
-                <MenuItem value='admin'>Admin</MenuItem>
-                <MenuItem value='author'>Author</MenuItem>
-                <MenuItem value='editor'>Editor</MenuItem>
-                <MenuItem value='maintainer'>Maintainer</MenuItem>
-                <MenuItem value='subscriber'>Subscriber</MenuItem>
-              </Select>
-            </FormControl>
+            <Controller
+              name="lastName"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  label="Last Name"
+                  placeholder=""
+                />
+              )}
+            />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <FormControl fullWidth>
-              <InputLabel>Status</InputLabel>
-              <Select label='Status' defaultValue='active'>
-                <MenuItem value='active'>Active</MenuItem>
-                <MenuItem value='inactive'>Inactive</MenuItem>
-                <MenuItem value='pending'>Pending</MenuItem>
-              </Select>
-            </FormControl>
+            <Controller
+              name="email"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  type="email"
+                  label="Email"
+                  placeholder=""
+                />
+              )}
+            />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField fullWidth label='Company' placeholder='ABC Pvt. Ltd.' defaultValue='ABC Pvt. Ltd.' />
+            <Controller
+              name="phoneNumber"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  type="tel"
+                  label="Phone Number"
+                  placeholder=""
+                />
+              )}
+            />
           </Grid>
-
-          {openAlert ? (
-            <Grid item xs={12} sx={{ mb: 3 }}>
-              <Alert
-                severity='warning'
-                sx={{ '& a': { fontWeight: 400 } }}
-                action={
-                  <IconButton size='small' color='inherit' aria-label='close' onClick={() => setOpenAlert(false)}>
-                    <Close fontSize='inherit' />
-                  </IconButton>
-                }
-              >
-                <AlertTitle>Your email is not confirmed. Please check your inbox.</AlertTitle>
-                <Link href='/' onClick={(e: SyntheticEvent) => e.preventDefault()}>
-                  Resend Confirmation
-                </Link>
-              </Alert>
-            </Grid>
-          ) : null}
+          <Grid item xs={12} sm={6}>
+            <Controller
+              name="role"
+              control={control}
+              render={({ field }) => (
+                <TextField {...field} disabled fullWidth label="Role" placeholder="" />
+              )}
+            />
+          </Grid>
 
           <Grid item xs={12}>
-            <Button variant='contained' sx={{ marginRight: 3.5 }}>
+            <Button type="submit" variant="contained" sx={{ marginRight: 3.5 }}>
               Save Changes
             </Button>
-            <Button type='reset' variant='outlined' color='secondary'>
+            <Button type="reset" variant="outlined" color="secondary">
               Reset
             </Button>
           </Grid>
         </Grid>
       </form>
     </CardContent>
-  )
-}
+  );
+};
 
-export default TabAccount
+export default TabAccount;
